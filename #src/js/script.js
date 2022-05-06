c1b410f02b53bf0442b9f107898c6aa54d18dc97
Ryan Johnson <ryan@ryanpainterdesign.com>
2022-05-06 14:24:45 -0400

    update for animations

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,18 +1,18 @@
-// const runAnimation = () => {
-//   let animationObjs = document.querySelectorAll(".dashboard");
-//   let staticObj = document.querySelector(".static");
-//   for (let animationObj of animationObjs) {
-//     if (staticObj !== animationObj) {
-//       let addActive = () => {
-//         animationObj.classList.add("active");
-//         //setTimeout(removeActive, 9200);
-//       };
-//       let removeActive = () => {
-//         animationObj.classList.remove("active");
-//       };
-//       addActive();
-//       setInterval(addActive, 9300);
-//     }
-//   }
-// };
-// runAnimation();
+const runAnimation = () => {
+  let animationObjs = document.querySelectorAll(".dashboard");
+  let staticObj = document.querySelector(".static");
+  for (let animationObj of animationObjs) {
+    if (staticObj !== animationObj) {
+      let addActive = () => {
+        animationObj.classList.add("active");
+        //setTimeout(removeActive, 9200);
+      };
+      let removeActive = () => {
+        animationObj.classList.remove("active");
+      };
+      addActive();
+      setInterval(addActive, 9300);
+    }
+  }
+};
+runAnimation();
